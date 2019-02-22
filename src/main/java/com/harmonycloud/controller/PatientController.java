32f9aa54bdfd80a8c4daa40c7ab5c2c99c954152
qidong <qidong@harmonycloud.cn>
2019-02-22 15:53:47 +0800

    update controller

--- src/main/java/com/harmonycloud/controller/PatientController.java
+++ src/main/java/com/harmonycloud/controller/PatientController.java
@@ -9,13 +9,14 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.Map;
-@Api(value = "病患controller", tags = {"病患操作接口"})
+@Api(tags = "Patient")
 @RestController
 public class PatientController {
 
@@ -23,15 +24,15 @@ public class PatientController {
     private PatientService patientService;
 
 
-    @ApiOperation(value = "注册病患")
-    @ApiImplicitParam(name = "patientAndPerson", value = "病患资料的实体类", required = true, dataType = "PatientAndPerson")
+    @ApiOperation(value = "register patient", httpMethod = "POST")
+    @ApiImplicitParam(name = "patientAndPerson", value = "patient entity", required = true, dataType = "PatientAndPerson")
     @PostMapping("/register")
     public Result register(@ApiParam @RequestBody PatientAndPerson patientAndPerson) {
         return patientService.register(patientAndPerson);
     }
 
-    @ApiOperation(value = "获取病患资料")
-    @ApiImplicitParam(name = "param", value = "获取病患资料的参数", required = true, dataType = "Map")
+    @ApiOperation(value = "get patient list", httpMethod = "POST")
+    @ApiImplicitParam(name = "param", value = "{\"searchData\":\"1\"}", required = true, dataType = "Map")
     @PostMapping("/patient")
     public Result getPatient(@RequestBody Map<String,String> param) {
         String searchData = param.get("searchData");
@@ -41,8 +42,8 @@ public class PatientController {
         return patientService.getPatient(searchData);
     }
 
-    @ApiOperation(value = "更新病患资料")
-    @ApiImplicitParam(name = "patientAndPerson", value = "病患资料的实体类", required = true, dataType = "PatientAndPerson")
+    @ApiOperation(value = "update patient", httpMethod = "POST")
+    @ApiImplicitParam(name = "patientAndPerson", value = "patient and person", required = true, dataType = "PatientAndPerson")
     @PostMapping("/update")
     public Result updatePatient(@RequestBody PatientAndPerson patientAndPerson) {
         return patientService.updatePatient(patientAndPerson);
