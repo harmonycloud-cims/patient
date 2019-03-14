d711b362e116e8d41e92bcca02617f66efb7becb
dongqi <dongqi@harmonycloud.cn>
2019-03-14 17:24:59 +0800

    delete inline tomcat

--- src/main/java/com/harmonycloud/main/PatientApplication.java
+++ src/main/java/com/harmonycloud/main/PatientApplication.java
@@ -3,6 +3,8 @@ package com.harmonycloud.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,7 +16,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan("com.harmonycloud")
 @EnableJpaRepositories("com.harmonycloud.repository")
 @EntityScan("com.harmonycloud.entity")
-public class PatientApplication {
+public class PatientApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PatientApplication.class, args);
